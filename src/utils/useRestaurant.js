42c23c15c3cd71f83bc1137dfa90d1c77b5adf98
Eekshit Chawla <eekshitchawla@eeki.local>
2023-12-08 23:15:20 +0530

    vercel final

--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
 import { API_PREFIX_URL, API_SUFFIX_URL } from "./constants";
 
-const useRestaurant = (resId) =>{
-    const [resInfo, setResInfo] = useState(null);
+const useRestaurant = (resId) => {
+  const [resInfo, setResInfo] = useState(null);
 
-    useEffect(() => {
-        fetchData();
-      }, []);
-    
-      const fetchData = async () => {
-        try {
-          const data = await fetch(API_PREFIX_URL + resId + API_SUFFIX_URL);
-          const json = await data.json();
-          setResInfo(json?.data);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-      return resInfo;
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch(API_PREFIX_URL + resId + API_SUFFIX_URL);
+      const json = await data.json();
+      setResInfo(json?.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  return resInfo;
+};
 
 export default useRestaurant;
